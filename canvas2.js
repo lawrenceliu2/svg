292532e2fa4de5a0f97d60b01c627d108a6e7a5b
Lawrence Liu <lawrence255@yahoo.com>
2017-03-01 13:04:58 -0500

    sanic speeds

--- canvas2.js
+++ canvas2.js
@@ -1,8 +1,11 @@
 var pic = document.getElementById("vimage");
 var b = document.getElementById("b");
+var g = document.getElementById("grow");
 var lastXcor;
 var lastYcor;
 var first = true;
+var rad = 1;
+var limit = false;
 
 var createDot = function() {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -33,15 +36,33 @@ var createDot = function() {
 
 pic.addEventListener("click", createDot);
 
+//Cheating method
 var clear = function() {
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    c.setAttribute("x", 0);
-    c.setAttribute("y", 0);
-    c.setAttribute("width", 500);
-    c.setAttribute("height", 500);
-    c.setAttribute("fill", "white");
+    while (pic.childNodes.length > 0){
+	pic.removeChild(pic.childNodes[0]);
+    }
     first = true;
-    pic.appendChild(c);
 };
 
 b.addEventListener("click", clear);
+
+var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+c.setAttribute("cx", 250);
+c.setAttribute("cy", 250);
+c.setAttribute("r", rad);
+c.setAttribute("fill", "blue");
+c.setAttribute("stroke", "black");
+
+var animateDot = function(){
+    console.log(rad);
+    //clear();
+    c.setAttribute("r", rad);
+    pic.appendChild(c);
+    if (rad == 1){limit = false;}
+    if (rad == 250){limit = true;}
+    if (limit){rad--;}
+    else{rad++;}
+    intervalID = window.setInterval(animateDot,160);
+};
+
+g.addEventListener("click", animateDot);
