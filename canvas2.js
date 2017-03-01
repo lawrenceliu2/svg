8261927e14294d97edfaca6bc909b22d570377d0
Lawrence Liu <lawrence255@yahoo.com>
2017-02-28 19:50:18 -0500

    clearing works

--- canvas2.js
+++ canvas2.js
@@ -34,7 +34,14 @@ var createDot = function() {
 pic.addEventListener("click", createDot);
 
 var clear = function() {
-    
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    c.setAttribute("x", 0);
+    c.setAttribute("y", 0);
+    c.setAttribute("width", 500);
+    c.setAttribute("height", 500);
+    c.setAttribute("fill", "white");
+    first = true;
+    pic.appendChild(c);
 };
 
 b.addEventListener("click", clear);
